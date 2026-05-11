44e3e60ae88055aafeb4487c7125bce5533e63d8
Darrigannn <aml207@alu.ua.es>
2026-05-11 15:24:19 +0200

    Update practica2.py

--- practica2.py
+++ practica2.py
@@ -3,7 +3,7 @@ import numpy as np
 import copy
 import os
 
-def preprocess_point_cloud(pcd, voxel_size):
+def preprocess_point_cloud(pcd, voxel_size,vecindad_normal,vecindad_descriptores, max_norm, max_vecin):
     """
     Realiza el filtrado (subsampling), cálculo de normales y cálculo de descriptores.
     """
@@ -12,17 +12,15 @@ def preprocess_point_cloud(pcd, voxel_size):
     pcd_down = pcd.voxel_down_sample(voxel_size)
 
     #(3 Extraer puntos caracteristicos) 2. Extraer características (Cálculo de normales)
-    radius_normal = voxel_size * 2
-    print(f"    -> Calculando normales con radio de búsqueda: {radius_normal}")
+    print(f"    -> Calculando normales con radio de búsqueda: {vecindad_normal}")
     pcd_down.estimate_normals(
-        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+        o3d.geometry.KDTreeSearchParamHybrid(radius=vecindad_normal, max_nn=max_norm))
 
     #(4 Calcular los descriptores para los puntos característicos) 3. Calcular descriptores (FPFH - Fast Point Feature Histograms)
-    radius_feature = voxel_size * 5
-    print(f"    -> Calculando descriptores FPFH con radio: {radius_feature}")
+    print(f"    -> Calculando descriptores FPFH con radio: {vecindad_descriptores}")
     pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
         pcd_down,
-        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+        o3d.geometry.KDTreeSearchParamHybrid(radius=vecindad_descriptores, max_nn=max_vecin))
     
     return pcd_down, pcd_fpfh
 
@@ -61,13 +59,57 @@ def refine_registration(source_down, target_down, initial_transform, voxel_size)
     return result
 
 def main():
+    cloud_seleccion = 1
+    
     # Rutas de los archivos (ajusta las rutas según dónde tengas los ficheros descomprimidos)
-    SCENE_PATH = "clouds/scenes/snap_0point.pcd"
-    OBJECTS_DIR = "clouds/objects/"
+    
+    if cloud_seleccion == 1:
+        SCENE_PATH = "clouds/scenes/snap_0point.pcd"
+        OBJECTS_DIR = "clouds/objects/"
+        object_files = ["s0_mug_corr.pcd", "s0_plc_corr.pcd", "s0_plant_corr.pcd", "s0_piggybank_corr.pcd"]
+    elif cloud_seleccion == 2:
+        SCENE_PATH = "clouds_new/clouds/scenes/snap_0point.pcd"
+        OBJECTS_DIR = "clouds_new/clouds/objects/"
+        object_files = ["s0_mug_corr.pcd", "s0_plc_corr.pcd", "s0_plant_corr.pcd", "s0_piggybank_corr.pcd"]
+    elif cloud_seleccion == 3:
+        SCENE_PATH = "more_clouds/clutter_scene/pcd_26.pcd"
+        OBJECTS_DIR = "more_clouds/objects/"
+        object_files = ["pcd_9.pcd", "pcd_21.pcd", "pcd_33.pcd"]
+    elif cloud_seleccion == 4:
+        SCENE_PATH = "more_clouds/pepper_scene/pcd_21.pcd"
+        OBJECTS_DIR = "more_clouds/objects/"
+        object_files = ["pcd_9.pcd", "pcd_21.pcd", "pcd_33.pcd"]
+
+    
     
     # Parámetro clave: tamaño del voxel. Si los puntos están en metros, 0.005 son 5 milímetros.
     # ¡Prueba a cambiar este valor para tu memoria!
-    VOXEL_SIZE = 0.005 
+    #PARAMETROS INVOLUCRADOS EN EL TIMELINE
+    VOXEL_SIZE = 0.005 #Tamano del voxel
+
+    VECINDAD_NORMAL = VOXEL_SIZE * 2 #Vecindad para calcular las normales
+    VECIN_NORM_MAX = 30 #Maximo de vecinos de la normal
+
+    VECINDAD_DESCRIPTORES = VOXEL_SIZE * 5 #Vecindad para calcular los descriptores
+    VECIN_DESC_MAX = 100 #Maximo de vecinos de los descriptores
+
+    SAMPLES_RANSAC = 3 #Numero de samples para RANSAC
+    UMBRAL_RANSAC = 2 #Umbral de aceptacion para ransac
+    UMBRAL_ICP = 2 #Umbral de aceptacion para icp
+
+
+
+
+    vecinidad calculo de normales: radius_normal
+maximo vecinos normales: max_nn
+
+vecinidad calculo de descriptores:radius_feature
+maximo vecinos descriptores: max_nn
+
+Numero de samples para RANSAC: ransac_n
+umbral de aceptacion para RANSAC: distance_threshold
+
+Umbral de acepctación para ICP: distance_threshold 
 
     print("=== 1. CARGANDO Y PREPROCESANDO LA ESCENA ===")
     scene = o3d.io.read_point_cloud(SCENE_PATH)
@@ -86,14 +128,11 @@ def main():
     scene_no_plane = scene.select_by_index(inliers, invert=True)
     
     # Preprocesamos la escena (downsample, normales, FPFH)
-    scene_down, scene_fpfh = preprocess_point_cloud(scene_no_plane, VOXEL_SIZE)
+    scene_down, scene_fpfh = preprocess_point_cloud(scene_no_plane, VOXEL_SIZE,VECINDAD_NORMAL,VECINDAD_DESCRIPTORES,VECIN_NORM_MAX,VECIN_DESC_MAX)
 
     # Nube final que usaremos para visualizar (una copia para no modificar la original)
     final_visualization = copy.deepcopy(scene)
     #final_visualization.paint_uniform_color([0.8, 0.8, 0.8]) # Escena en gris claro
-
-    # Lista de archivos de objetos a buscar
-    object_files = ["s0_mug_corr.pcd", "s0_plc_corr.pcd", "s0_plant_corr.pcd", "s0_piggybank_corr.pcd"]
     
     # Colores llamativos para cada objeto encontrado
     colors = [
@@ -139,6 +178,6 @@ def main():
     print("\n=== 3. VISUALIZACIÓN FINAL ===")
     # Visualizamos la escena procesada (sin planos) junto con los objetos encontrados pintados
     o3d.visualization.draw_geometries([final_visualization], window_name="Reconocimiento de Objetos 3D", width=1024, height=768)
-
+    #o3d.visualization.draw_geometries([scene], window_name="Reconocimiento de Objetos 3D", width=1024, height=768)
 if __name__ == "__main__":
     main()
